d66cb776aa7d3c58c035ab2f7808e0badacabf6d
Filipe Varela <keoshi@booya.lan>
2015-05-28 16:28:16 +0100

    Replaces stats markup for component

--- components/stats-charts/index.jsx
+++ components/stats-charts/index.jsx
@@ -17,12 +17,8 @@ Stats_Charts = React.createClass( {
 			<div className="stats__module stats__graphs">
 			
 				<div className="chart-circular__block chart-circular__one">
-
-					<div className="chart-circular__data">
-						<span className="numbers__value value-primary value-chart">43%</span>
-						<span className="numbers__description">Desktop views</span>
-						<span className="numbers__trend trend-negative trend-center">16%</span>
-					</div>
+			
+					<Stats_Numbers className="chart-circular__data" type="chart" value={43} trend={16} description="Desktop Views" />
 		
 					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 159.6 159.6" enable-background="new 0 0 159.6 159.6">
 						<path className="chart-graph-dummy" fill="none" stroke="#d8e2e9" strokeWidth="6" d="M79.8,3c42.4,0,76.8,34.4,76.8,76.8 s-34.4,76.8-76.8,76.8S3,122.2,3,79.8S37.4,3,79.8,3"/>
@@ -32,12 +28,8 @@ Stats_Charts = React.createClass( {
 				</div>
 			
 				<div className="chart-circular__block chart-circular__two">
-			
-					<div className="chart-circular__data">
-						<span className="numbers__value value-secondary value-chart">57%</span>
-						<span className="numbers__description">Mobile views</span>
-						<span className="numbers__trend trend-positive trend-center">22%</span>
-					</div>
+					
+					<Stats_Numbers className="chart-circular__data" type="chart" value={82} trend={28} description="Mobile Views" />
 		
 					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 159.6 159.6" enable-background="new 0 0 159.6 159.6">
 						<path className="chart-graph-dummy" fill="none" stroke="#d8e2e9" strokeWidth="6" d="M79.8,3c42.4,0,76.8,34.4,76.8,76.8 s-34.4,76.8-76.8,76.8S3,122.2,3,79.8S37.4,3,79.8,3"/>
