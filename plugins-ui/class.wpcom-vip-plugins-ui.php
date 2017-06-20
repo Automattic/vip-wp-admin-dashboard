1ad9c568e86489d14a9e738a60a811b2ebcf9cf9
Scott Evans <git@scott.ee>
2017-06-20 11:40:39 +0100

    Remove Find The Best

--- plugins-ui/class.wpcom-vip-plugins-ui.php
+++ plugins-ui/class.wpcom-vip-plugins-ui.php
@@ -177,11 +177,6 @@ class WPCOM_VIP_Plugins_UI {
 				'name'        => 'CoSchedule',
 				'description' => 'Plan awesome content. Save a bunch of time.',
 			),
-			'findthebest'       => array(
-				'name'        => 'FindTheBest',
-				'description' => 'Add visual, interactive content that matches your post and
-				boosts your credibility.',
-			),
 			'getty-images'       => array(
 				'name'        => 'Getty Images',
 				'description' => 'Search and use Getty Images photos in your posts without ever leaving WordPress.com.',
