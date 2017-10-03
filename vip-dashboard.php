d20da0dbdd9d93da361d096486e18c2811e2944f
Scott Evans <git@scott.ee>
2017-10-03 19:41:06 +0100

    Begin retirement of vip plugins ui

--- vip-dashboard.php
+++ vip-dashboard.php
@@ -19,7 +19,7 @@
  * Despite it's name, the class is also responsible for loading the plugins on both
  * frontend and backend, so it always has to be loaded and initialised
  */
-require __DIR__ . '/plugins-ui/plugins-ui.php';
+// require __DIR__ . '/plugins-ui/plugins-ui.php';
 
 /**
  * Boot the new VIP Dashboard
@@ -36,8 +36,6 @@ function vip_dashboard_init() {
 	add_action( 'admin_menu', 'wpcom_vip_admin_menu', 5 );
 	add_action( 'admin_menu', 'wpcom_vip_rename_vip_menu_to_dashboard', 50 );
 
-	// Remove standard WP plugins screen.
-	add_action( 'admin_menu', 'vip_dashboard_remove_menu_pages' );
 }
 add_action( 'plugins_loaded', 'vip_dashboard_init' );
 
@@ -59,15 +57,6 @@ function vip_dashboard_admin_styles() {
 function vip_dashboard_admin_scripts() {
 	wp_register_script( 'vip-dashboard-script', plugins_url( '/assets/js/vip-dashboard.js', __FILE__ ), array( 'jquery' ), '1.0', true );
 	wp_enqueue_script( 'vip-dashboard-script' );
-}
-
-/**
- * Remove plugins menu item for all but vip_support
- *
- * @return void
- */
-function vip_dashboard_remove_menu_pages() {
-	remove_menu_page( 'plugins.php' );
 }
 
 /**
@@ -346,3 +335,86 @@ function wpcom_vip_menu_order( $menu_ord ) {
 
 	return $vip_order;
 }
+
+function wpcom_vip_featured_plugins() {
+	global $hook_suffix;
+
+	if ( 'plugins.php' !== $hook_suffix ) {
+		return;
+	}
+
+	// todo: integrate with API
+	?>
+	<div class="featured-plugins notice" style="background: #fff; padding: 20px; margin: 20px 0; clear: both;">
+		<span>Featured partner plugins</span>
+	</div>
+	<?php
+}
+add_action( 'admin_notices', 'wpcom_vip_featured_plugins', 99 );
+
+/**
+ * Returns a filtered list of code activated plugins similar to core plugins Option
+ *
+ * @return array list of filtered plugins
+ */
+function wpcom_vip_get_filtered_loaded_plugins() {
+	$code_plugins = wpcom_vip_get_loaded_plugins();
+	foreach ( $code_plugins as $key => $plugin ) {
+		if ( strpos( $plugin, 'shared-plugins' ) !== false ) {
+			unset( $code_plugins[ $key ] );
+		} else {
+			$code_plugins[ $key ] = str_replace( 'plugins/', '', $plugin );
+		}
+	}
+
+	return $code_plugins;
+}
+
+/**
+ * Ensure code activated plugins are shown as such on core plugins screens
+ *
+ * @param  array $actions
+ * @param  string $plugin_file
+ * @param  array $plugin_data
+ * @param  string $context
+ * @return array
+ */
+function wpcom_vip_plugin_action_links( $actions, $plugin_file, $plugin_data, $context ) {
+	if ( in_array( $plugin_file, wpcom_vip_get_filtered_loaded_plugins(), true ) ) {
+		$actions['deactivate'] = __( 'Enabled via code', 'vip-dashboard' );
+	}
+
+	// plugins cannot be deleted on VIP Go
+	unset( $actions['delete'] );
+
+	return $actions;
+}
+add_filter( 'plugin_action_links', 'wpcom_vip_plugin_action_links', 10, 4 );
+
+/**
+ * Merge code activated plugins with database option for better UI experience
+ *
+ * @param  array $value
+ * @param  string $option
+ * @return array
+ */
+function wpcom_vip_option_active_plugins( $value, $option ) {
+	$code_plugins = wpcom_vip_get_filtered_loaded_plugins();
+	$value = array_merge( $code_plugins, $value );
+
+	return $value;
+}
+add_filter( 'option_active_plugins', 'wpcom_vip_option_active_plugins', 10, 2 );
+
+/**
+ * Disable ability to bulk update or delete plugins
+ * @param  array $bulk_actions
+ * @return array
+ */
+function wpcom_vip_bulk_actions_plugins( $bulk_actions ) {
+	unset( $bulk_actions['delete-selected'] );
+	unset( $bulk_actions['update-selected'] );
+
+	return $bulk_actions;
+}
+add_filter( 'bulk_actions-plugins', 'wpcom_vip_bulk_actions_plugins', 10, 1 );
